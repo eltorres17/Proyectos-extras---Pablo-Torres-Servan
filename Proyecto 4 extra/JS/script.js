aff1885327f148b1968892879ae51e14c8ac0534
eltorres17 <pablotorreservan@gmail.com>
2025-02-17 18:59:05 +0100

    Proyecto 5 terminado

--- Proyecto 4 extra/JS/script.js
+++ Proyecto 4 extra/JS/script.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("principal__inicio__boton").addEventListener("click", añadir);
+    document.getElementById("principal__inicio__aleatorio").addEventListener("click", aleatorio);
 
     function añadir() {
         let principal__inicio__texto = document.getElementById("principal__inicio__texto");
@@ -23,5 +24,20 @@ document.addEventListener("DOMContentLoaded", function() {
         } else {
             alert("No se pueden añadir tareas vacías.");
         }
+    }
+
+    function aleatorio() {
+        let lista = document.getElementById("Lista");
+        let tareas = lista.getElementsByTagName("li");
+        if (tareas.length > 0){
+            let aleatorio = Math.floor(Math.random()* tareas.length);
+            for (let tarea of tareas) {
+                 tareas[aleatorio].classList.add("seleccionado");
+            }
+            tareas.classList.add("sleccionado");
+        }else{
+            alert("No hay tareas para seleccionar");
+        }
+    
     }
 });
